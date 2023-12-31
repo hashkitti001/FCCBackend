86b077040c41068d9d29f4a423b1eac434247966
hashkitti001 <iJeddy5.0@gmail.com>
2023-12-31 19:04:39 +0100

    Used dotenv to extract data from an .env file

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+const dotenv = require("dotenv").config()
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 absolutePath = __dirname + "/views/index.html"
@@ -7,6 +8,8 @@ app.get("/", (req, res) => {
     res.sendFile(absolutePath);  
 })
 app.get("/json", (req,res) => {
+    process.env.MESSAGE_STYLE == "uppercase"?   
+    res.status(200).json({"message": "HELLO JSON"}):
     res.status(200).json({"message": "Hello json"})
 })
 
