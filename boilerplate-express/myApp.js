e15f8fbe3d65a4313478f58a1565bdd98e8cc1cd
hashkitti001 <iJeddy5.0@gmail.com>
2023-12-31 19:56:34 +0100

    Implemented middleware function for every request

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -3,16 +3,19 @@ let app = express();
 const dotenv = require("dotenv").config()
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+     //Log a string with request method, path and ip
+     console.log(`${req.method} ${req.path} - ${req.ip}`)
+     next()
+})
 absolutePath = __dirname + "/views/index.html"
 app.get("/", (req, res) => {
     res.sendFile(absolutePath);  
 })
-app.get("/json", (req,res, next) => {
+app.get("/json", (req,res) => {
    let msg =  process.env.MESSAGE_STYLE === "uppercase"? "HELLO JSON" : "Hello json"
-    // res.status(200).json({"message": msg})
-    //Log a string with request method, path and ip
-    console.log(`${req.method} ${req.path} - ${req.ip}`)
-    next()
+    res.status(200).json({"message": msg})
+   
 })
 
 
