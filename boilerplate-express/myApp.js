8ec1389f3289365b114dae44c7084555feac7a8a
hashkitti001 <iJeddy5.0@gmail.com>
2023-12-31 18:16:34 +0100

    Made Express server serve static view

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
+absolutePath = __dirname + "/views/index.html"
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
+    
 })
 
 
