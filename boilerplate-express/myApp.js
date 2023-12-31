6e77f349541d787e072f975269f40a5462c571a6
hashkitti001 <iJeddy5.0@gmail.com>
2023-12-31 18:36:31 +0100

    Made server serve static files

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,12 +1,11 @@
 let express = require('express');
 let app = express();
+app.use(express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"))
 absolutePath = __dirname + "/views/index.html"
 app.get("/", (req, res) => {
-    res.sendFile(absolutePath);
-    
+    res.sendFile(absolutePath);  
 })
-console.log(absolutePath)
-
 
 
 
