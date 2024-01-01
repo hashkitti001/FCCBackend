7e2049c39fcf508d9f2ec9307c9f450ae3e34e34
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-01 02:40:47 +0100

    Initialize database connection

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const mongoose = require("mongoose")
-let connect = async () => {
- await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology:true})
- console.log("Connected")
+async function main() {
+  let uri = process.env.MONGO_URI;
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  console.log("Connected to", uri);
 }
-connect().catch(err => console.error(err))
+main().catch(err => console.error(err))
 let Person;
 
 const createAndSavePerson = (done) => {
