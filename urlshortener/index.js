f27ced58ff4ff06b8dfbe7b96da71768762b2dd4
hashkitti001 <iJeddy5.0@gmail.com>
2024-02-20 14:00:01 +0100

    fixed get and post endpoints

--- urlshortener/index.js
+++ urlshortener/index.js
@@ -6,77 +6,73 @@ const app = express();
 const mongoose = require("mongoose")
 const Url = require("./urlModel")
 const dns = require("node:dns")
+const urlparser = require("node:url")
 // Basic Configuration
 const port = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 //Connects to the database
 let dbConn = async () => {
   try {
-  await mongoose.connect(process.env.MONGO_URI)
-  console.log("Connected successfully")
-  
-}
-  catch(e) {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected successfully")
+
+  }
+  catch (e) {
     console.log(e)
   }
 }
 dbConn()
-const generateURLId = () => {
-  return Math.floor(Math.random() * 1000)
-}
-app.get('/api/shorturl/:id', async(req, res) => {
+app.get('/api/shorturl/:id', async (req, res) => {
+      
   try {
-     let shortURLId = parseInt(req.params.id, 10)
-     const ans = await Url.findOne({id: shortURLId})
-    if(ans != null && !(ans.longurl.startsWith("http"))){
-      res.redirect(`https://${ans.longurl}`)
-    }
-    else if(ans != null && ans.longurl.startsWith("http")){
-       res.redirect(ans.longurl)
-    }
-    else {
-      res.json({message: "No such URL on this service"})
-    }
-    // res.redirect("https://www.google.com")
-  } catch(e){
-        console.error("Something went wrong", e)
-        res.status(500).json({message: "Internal server error"})
+    let foundURL = await Url.findOne({identifier: req.params.identifier})
+  
+    res.redirect(foundURL.longurl)
+  } catch (e) {
+    console.error("Something went wrong", e)
+    res.status(500).json({ message: "Internal server error" })
   }
 })
-app.post('/api/shorturl', async(req, res) => {
-  try { 
+app.post('/api/shorturl', async (req, res) => {
+  try {
     let { url } = req.body
-    dns.lookup(url, async (err, address) => {
-      if(err){
-        res.json({message: "Try another URL or complete this one"})
-      }
-      else {
-        const shortURLId = generateURLId()
-        const newURL = new Url({
-          "id": shortURLId,
-          "longurl": url
-        })
-        await newURL.save()
-        res.status(201).json({
-          original_url: url, 
-          short_url: shortURLId
-        })
-      }
-    })
-    
+    const dnslookup = dns.lookup(
+      new URL(url).hostname,
+      async (err, addr) => {
+        if (!addr) {
+          res.json({ error: "Invalid URL" })
+        }
+        else {
+          const previousURLs = await Url.find({})
+          const urlCount = previousURLs.length
+          const newURL = new Url({
+            identifier: urlCount,
+            longurl: req.body.url
+          })
+          await newURL.save()
+          res.status(201).json({
+            original_url: req.body.url,
+            short_url: urlCount
+          })
+
+        }
+      })
+
   }
-catch(e) {
- res.status(500).json({message:"Internal Server Error"})
-}
+
+  catch (e) {
+    res.status(500).json({ message: "Internal Server Error" })
+    console.error(e)
+  }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
